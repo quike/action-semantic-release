0c201935fdebb5442ac4b56d8327a130edb8cb35
quike <quike.mora@gmail.com>
2025-02-20 14:36:22 -0500

    fix: set proper default file location (#5)

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ config() {
   npm link @actions/core
   bash -c "git config --global --add safe.directory \$PWD"
   CONFIG_EXISTS=false
-  DEFAULT_CONFIG="/usr/src/config/.releaserc.default"
+  DEFAULT_CONFIG="/.releaserc.default"
   CONFIG_FILES=".releaserc .releaserc.json .releaserc.yaml .releaserc.yml .releaserc.js .releaserc.cjs release.config.js release.config.cjs"
   for CONF_FILE in ${CONFIG_FILES}; do
     if [ -e "${CONF_FILE}" ]; then
