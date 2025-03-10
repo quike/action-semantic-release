bbae94488e83b599beab2d69546b4e48d3af5f81
quike <quike.mora@gmail.com>
2025-03-10 13:18:32 -0400

    fix: correct wrong import

--- src/set-floating-tags.js
+++ src/set-floating-tags.js
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
 import { execa } from 'execa'
-import { DEFAULT_USER } from './constants'
+import { DEFAULT_USER } from './constants.js'
 
 /**
  * Sets floating tags for the release.
