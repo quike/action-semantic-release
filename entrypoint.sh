ff4fed9bb30b1b99f1861f2b7716dd2220a30946
quike <616137+quike@users.noreply.github.com>
2025-04-30 13:46:14 -0400

    feat: add metadata file to export variables (#51)

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,14 +55,38 @@ validate() {
   echo "git-head=${CURRENT_SHA}" >>"$GITHUB_OUTPUT"
   echo "git-head=${CURRENT_SHA}" >>"$GITHUB_ENV"
   if [ -n "$EVENT_NAME" ] && [ "$EVENT_NAME" == "pull_request" ]; then
-    echo "Type of event: $EVENT_NAME"
+    print "Type of event: $EVENT_NAME"
     ORIGINAL_SHA=$(jq -r '.pull_request.head.sha' "$GITHUB_EVENT_PATH")
-    echo "Original Pull Request Commit ID: $ORIGINAL_SHA"
-    echo "HEAD is now at ${CURRENT_SHA} after Pull Request Commit Id merge with HEAD."
+    print "Original Pull Request Commit ID: $ORIGINAL_SHA"
+    print "HEAD is now at ${CURRENT_SHA} after Pull Request Commit Id merge with HEAD."
     PULL_REQUEST_VERSION=${ORIGINAL_SHA:0:7}
-    echo "Short SHA=${PULL_REQUEST_VERSION}"
+    print "Short SHA=${PULL_REQUEST_VERSION}"
     echo "release-version=${PULL_REQUEST_VERSION}" >>"$GITHUB_OUTPUT"
     echo "release-version=${PULL_REQUEST_VERSION}" >>"$GITHUB_ENV"
+  fi
+}
+
+export_metadata() {
+  INPUT_FILE="$GITHUB_ENV"
+  OUTPUT_FILE="$WORKING_PATH/metadata.env"
+  if [ -f "$INPUT_FILE" ] && [ -n "$EXPORT_METADATA" ] && [ "$EXPORT_METADATA" = true ]; then
+    true >"$OUTPUT_FILE"
+
+    while IFS= read -r line; do
+      if [[ "$line" == *"<<ghadelimiter_"* ]]; then
+        key="${line%%<<*}" # Get part before << delimiter
+        # Read value and delimiter end line
+        IFS= read -r value
+        IFS= read -r _end_delim
+        echo "$key=$value" >>"$OUTPUT_FILE"
+      fi
+    done <"$INPUT_FILE"
+    if [ -f "$OUTPUT_FILE" ]; then
+      print "Metadata file created successfully: $OUTPUT_FILE"
+    else
+      print "Error: Metadata file was not created: $OUTPUT_FILE"
+      exit 4
+    fi
   fi
 }
 
@@ -72,6 +96,7 @@ main() {
   config "$@"
   run "$@"
   validate "$@"
+  export_metadata "$@"
 }
 
-main "$@"
+export_metadata "$@"
